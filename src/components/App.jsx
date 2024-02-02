327f3bc1375e4b5e58415ad5530ed97ea42f535e
Alexander221991 <kurakoff.sanyaba2@gmail.com>
2024-02-02 21:26:15 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,22 @@
 import { nanoid } from 'nanoid';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { Notify } from 'notiflix';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+const LOCALSTORAGE_KEY = 'contacts-key';
 
-  componentDidMount() {
-    const savedState = localStorage.getItem('contacts-key');
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) ?? []
+  );
+  const [filter, setFilter] = useState('');
+  useEffect(() => {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
-    if (savedState) {
-      const savedContacts = JSON.parse(localStorage.getItem('contacts-key'));
-      this.setState({ contacts: savedContacts });
-    }
-  }
-  componentDidUpdate(_, prevState) {
-    const { contacts } = this.state;
-    if (prevState.contacts !== contacts) {
-      if (!contacts.length) {
-        localStorage.removeItem('contacts-key');
-      } else {
-        localStorage.setItem('contacts-key', JSON.stringify(contacts));
-      }
-    }
-  }
-
-  addContact = newContact => {
-    const { contacts } = this.state;
+  const addContact = newContact => {
     const newName = newContact.name.toLowerCase();
     const isCheckedContact = contacts.find(
       ({ name }) => name.toLowerCase() === newName
@@ -39,18 +24,17 @@ export class App extends Component {
 
     if (!isCheckedContact) {
       newContact.id = nanoid();
-      this.setState({ contacts: [...this.state.contacts, newContact] });
+      setContacts(prevContacts => [...contacts, newContact]);
     } else {
       Notify.failure(`${newContact.name} is already in contacts`);
     }
   };
 
-  findContact = ({ target: { value } }) => {
-    this.setState({ filter: value.toLowerCase() });
+  const findContact = ({ target: { value } }) => {
+    setFilter(value.toLowerCase());
   };
 
-  filteredContact = () => {
-    const { contacts, filter } = this.state;
+  const filteredContact = () => {
     const suitableContacts = contacts.filter(({ name }) =>
       name.toLowerCase().includes(filter)
     );
@@ -58,26 +42,22 @@ export class App extends Component {
     return filter === '' ? contacts : suitableContacts;
   };
 
-  deleteContact = idDeleteContact => {
-    this.setState({
-      contacts: this.state.contacts.filter(({ id }) => id !== idDeleteContact),
-    });
+  const deleteContact = idDeleteContact => {
+    setContacts(contacts.filter(({ id }) => id !== idDeleteContact));
   };
 
-  render() {
-    const { addContact, findContact, deleteContact, filteredContact } = this;
+  return (
+    <div className="container">
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <h2>Contacts</h2>
+      <Filter findContact={findContact} />
+      <ContactList
+        filteredContact={filteredContact()}
+        deleteContact={deleteContact}
+      />
+    </div>
+  );
+};
 
-    return (
-      <div className="container">
-        <h1>Phonebook</h1>
-        <ContactForm addContact={addContact} />
-        <h2>Contacts</h2>
-        <Filter findContact={findContact} />
-        <ContactList
-          filteredContact={filteredContact()}
-          deleteContact={deleteContact}
-        />
-      </div>
-    );
-  }
-}
+export default App;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,57 +1,50 @@
 import styles from './contact-form.module.css';
 
-import { Component } from 'react';
+import { useState } from 'react';
 
-export class ContactForm extends Component {
-  state = {
+export const ContactForm = ({ addContact }) => {
+  const [dataContact, setDataContact] = useState({
     name: '',
     number: '',
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setDataContact({ ...dataContact, [name]: value });
   };
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-
-    this.props.addContact({
-      ...this.state,
-    });
+    addContact({ ...dataContact });
     e.target.reset();
   };
 
-  render() {
-    return (
-      <form className={styles.form} onSubmit={this.handleSubmit}>
-        <label htmlFor="name" className={styles.label}>
-          Name
-          <input
-            className={styles.input}
-            type="text"
-            name="name"
-            id="name"
-            required
-            onChange={this.handleChange}
-          />
-        </label>
-        <label htmlFor="number" className={styles.label}>
-          Number
-          <input
-            className={styles.input}
-            type="tel"
-            name="number"
-            id="number"
-            required
-            onChange={this.handleChange}
-          />
-        </label>
-        <button className={styles.btn} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <label htmlFor="name" className={styles.label}>
+        Name
+        <input
+          className={styles.input}
+          type="text"
+          name="name"
+          id="name"
+          required
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="number" className={styles.label}>
+        Number
+        <input
+          className={styles.input}
+          type="tel"
+          name="number"
+          id="number"
+          required
+          onChange={handleChange}
+        />
+      </label>
+      <button className={styles.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
